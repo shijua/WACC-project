612a22c3e4ed8381ea123fab68e6b3c109671304
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-01-27 17:01:05 +0000

    feat: Defined AST for Expr

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,5 @@
+use crate::parser::lexer::Spanned;
+
 // Abstract Syntax Tree Node Specification
 #[derive(PartialEq, Clone, Debug)]
 pub enum UnaryOperator {
@@ -23,4 +25,64 @@ pub enum BinaryOperator {
     Neq,
     And,
     Or,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum BaseValue<'src> {
+    Null,
+    IntVal(i32),
+    BoolVal(bool),
+    CharVal(char),
+    StrVal(&'src str),
+    // ArrVal(Box<[Self]>),
+    // PairVal(Box<Self>, Box<Self>),
+}
+
+impl<'src> std::fmt::Display for BaseValue<'src> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Null => write!(f, "null"),
+            Self::IntVal(x) => write!(f, "{}", x),
+            Self::BoolVal(x) => write!(f, "{}", x),
+            Self::CharVal(x) => write!(f, "{}", x),
+            Self::StrVal(x) => write!(f, "{}", x),
+            // Self::ArrVal(xs) => write!(
+            //     f,
+            //     "[{}]",
+            //     xs.iter()
+            //         .map(|x| x.to_string())
+            //         .collect::<Vec<_>>()
+            //         .join(", ")
+            // ),
+            // Self::PairVal(p1, p2) => write!(f, "pair({}, {})", p1.to_string(), p2.to_string()),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct ArrayElem<'src>(pub &'src str, pub Vec<Spanned<Expr<'src>>>);
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum PairElem<'src> {
+    Fst(Spanned<Expr<'src>>),
+    Snd(Spanned<Expr<'src>>),
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum Expr<'src> {
+    Error,
+    BaseValue(BaseValue<'src>),
+    ArrayElem(ArrayElem<'src>),
+    // PairElem: i.e. NULL is integrated as BaseValue(BaseValue::Null)
+    Ident(&'src str),
+    UnaryApp(
+        UnaryOperator,
+        Box<Spanned<Expr<'src>>>,
+        Box<Spanned<Expr<'src>>>,
+    ),
+    BinaryApp(
+        Box<Spanned<Expr<'src>>>,
+        BinaryOperator,
+        Box<Spanned<Expr<'src>>>,
+    ),
 }
--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -1,1 +1,13 @@
 // <Expr> (expressions) syntax parser & analysis
+
+use crate::parser::lexer::{ParserInput, Span, Spanned, Token};
+use chumsky::error::Rich;
+use chumsky::{extra, Parser};
+
+// fn expr_parser<'tokens, 'src: 'tokens>() -> impl Parser<
+//     'tokens,
+//     ParserInput<'tokens, 'src>,
+//     Spanned<Expr<'src>>,
+//     extra::Err<Rich<'tokens, Token<'src>, Span>>,
+// > + Clone {
+// }
