2ce8b1d51a4be0118081b405c8e20008d6501ed2
Yi1lan <yanli.wang22@imperial.ac.uk>
2024-01-30 18:38:47 +0000

    feat: AST update: statements, program, function, lvalue, rvalue

--- src/ast.rs
+++ src/ast.rs
@@ -40,3 +40,104 @@ pub enum BinaryOperator {
     And,
     Or,
 }
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum BaseType {
+    Int,
+    Bool,
+    Char,
+    String,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum ArrType {
+    Arr(Box<Type>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum PairType {
+    Pair(Box<PairElemType>, Box<PairElemType>)
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum PairElemType {
+    PairElemBase(BaseType),
+    PairElemArr(ArrType), 
+    PairKeyword, 
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Type {
+    TypeBase(BaseType),
+    TypeArr(ArrType),
+    TypePair(PairType),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Stmt {
+    Skip,
+    Declare(Type, Expr, Rvalue),
+    Assign(Lvalue, Rvalue),
+    Read(Lvalue),
+    Free(Expr),
+    Return(Expr),
+    Exit(Expr),
+    Print(Expr),
+    Println(Expr),
+    If(Expr, Box<Stmt>, Box<Stmt>),
+    While(Expr, Box<Stmt>),
+    Begin(Box<Stmt>),
+    Serial(Box<Stmt>, Box<Stmt>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Lvalue {
+    LIdent(Expr),
+    LArrElem(Expr),
+    LPairElem(PairElem),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Rvalue {
+    RExpr(Expr),
+    RArrLit(ArrLit),
+    RNewPair(Box<Expr>, Box<Expr>),
+    RPairElem(PairElem),
+    RCall(Expr, ArgList),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum ArgList {
+    Arg(Vec<Expr>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum PairElem {
+    PairElemFst(Box<Lvalue>),
+    PairElemSnd(Box<Lvalue>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum ArrLit {
+    Lit(Vec<Expr>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Program {
+    Prog(Box<Function>, Box<Stmt>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Function {
+    Func(Type, Expr, ParamList, Box<Stmt>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum ParamList {
+    ParamL(Param, Box<Param>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Param {
+    Par(Type, Expr),
+}
