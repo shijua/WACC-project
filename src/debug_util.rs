e8ad784466ab01052955b5bab6608ffcff7aad6b
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-02-01 11:13:46 +0000

    chore: allow unused status in debugging utilities.

--- src/debug_util.rs
+++ src/debug_util.rs
@@ -1,16 +1,31 @@
-use crate::ast::Expr;
+#[allow(unused_imports)]
+use crate::ast::{BinaryOperator, Expr};
 
-pub fn unbox<T: Clone>(value: Box<T>) -> T {
+#[allow(dead_code)]
+fn unbox<T: Clone>(value: Box<T>) -> T {
     (*value).clone()
 }
 
-pub fn get_lhs_rhs (value: Expr) -> (Expr, Expr) {
+#[allow(dead_code)]
+pub fn get_lhs_rhs(value: Expr) -> (Expr, Expr) {
     match value {
         Expr::BinaryApp(left, _, right) => {
             let left = unbox(left);
             let right = unbox(right);
             (left, right)
-        },
-        _ => panic!("Not a binary app")
+        }
+        _ => unreachable!("Not a binary app"),
     }
+}
+
+#[test]
+fn test_debug_expr() {
+    assert!(matches!(
+        get_lhs_rhs(Expr::BinaryApp(
+            Box::new(Expr::IntLiter(1)),
+            BinaryOperator::Add,
+            Box::new(Expr::IntLiter(1))
+        )),
+        ((Expr::IntLiter(1), Expr::IntLiter(1)))
+    ));
 }
