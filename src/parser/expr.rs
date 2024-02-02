a3aef49f97e4550232ad011bde6e994ca824b291
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-02-02 14:06:01 +0000

    feat: fixed escaped char accompanied parsing.

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -1,3 +1,170 @@
+use crate::ast::Expr;
+use chumsky::error::Rich;
+use chumsky::prelude::{any, choice, just, none_of, one_of, SimpleSpan};
+use chumsky::{extra, text, IterParser, Parser};
+
 fn is_valid_single_ascii(chr: u8) -> bool {
     (chr >= 0x20 && chr < 0x80) && (chr != b'\'') && (chr != b'\"') && (chr != b'\\')
+}
+
+pub type Span = SimpleSpan<usize>;
+
+pub type Spanned<T> = (T, Span);
+
+fn expr_atom_literal<'a>(
+) -> impl Parser<'a, &'a str, Spanned<Expr>, extra::Err<Rich<'a, char, Span>>> {
+    // <int-liter> ::= ⟨'+' | '-'⟩? ⟨digit⟩+
+    let int_liter = one_of("+-")
+        .or_not()
+        .then(text::digits(10)) // accepting decimal digits only
+        .to_slice()
+        .padded()
+        .validate(|s: &str, e, emitter| {
+            let num = s.parse::<i32>();
+            if num.is_err() {
+                emitter.emit(Rich::custom(
+                    e.span(),
+                    "Integer Oversize: this is not a 32-bit integer.",
+                ))
+            }
+            num.unwrap_or(0)
+        })
+        .map(Expr::IntLiter);
+
+    let bool_liter = choice((
+        text::ascii::keyword("true")
+            .to_slice()
+            .padded()
+            .to(Expr::BoolLiter(true)),
+        text::ascii::keyword("false")
+            .to_slice()
+            .padded()
+            .to(Expr::BoolLiter(false)),
+    ));
+
+    // Regarding transformation:
+    // Ideally we would recognize a specific pattern for escape characters and perform correct analysis
+    // The character transformation only applies for <char-liter> and <str-liter>.
+    // This should be an ascii character, and it should not parse unicodes.
+
+    let graphic_ascii_char = any::<'a, &'a str, extra::Err<Rich<'a, char, Span>>>()
+        .filter(char::is_ascii)
+        .filter(|c| c >= &' ');
+
+    let normal_char = graphic_ascii_char.and_is(none_of("\\\'\""));
+
+    // <escaped-char> ::= ‘0’|‘b’|‘t’|‘n’|‘f’|‘r’|‘"’|‘'’|‘\’
+    let escape_char = just('\\').ignore_then(choice((
+        just('0').to('\0'),
+        just('b').to('\x08'),
+        just('t').to('\t'),
+        just('n').to('\n'),
+        just('f').to('\x0C'),
+        just('r').to('\r'),
+        just('\"').to('\"'),
+        just('\'').to('\''),
+        just('\\').to('\\'),
+    )));
+
+    let char_elem = normal_char
+        .or(escape_char)
+        .to_slice()
+        .validate(move |s: &str, e, emitter| {
+            let n = s.chars().count();
+            if n == 1 {
+                normal_char.parse(s).into_result().unwrap()
+            } else if n == 2 {
+                let try_escape = escape_char.parse(s);
+                return if try_escape.has_errors() {
+                    emitter.emit(Rich::custom(e.span(), "invalid escape character pattern"));
+                    ' '
+                } else {
+                    try_escape.into_result().unwrap()
+                };
+            } else {
+                emitter.emit(Rich::custom(e.span(), "invalid char: unaccepted pattern"));
+                return ' ';
+            }
+        });
+
+    // let char_str = normal_char.or(escape_char).to_slice()
+
+    let char_liter = char_elem
+        .delimited_by(just('\''), just('\''))
+        .padded()
+        .map(Expr::CharLiter);
+
+    let str_preprocess = char_elem.clone().repeated();
+
+    let str_parts = char_elem.repeated().collect::<String>();
+
+    let str_liter = str_preprocess
+        .to_slice()
+        .delimited_by(just('\"'), just('\"'))
+        .map(move |s| {
+            println!("{}", s);
+            let result = str_parts.parse(s).into_result().unwrap();
+            Expr::StrLiter(result)
+        });
+
+    let token = int_liter.or(bool_liter).or(char_liter).or(str_liter);
+
+    token.map_with(|tok, e| (tok, e.span()))
+}
+
+#[test]
+fn can_parse_basic_int_liter() {
+    let input = "3";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(matches!(result, Ok((expr, _)) if expr == Expr::IntLiter(3)));
+}
+
+#[test]
+fn cannot_parse_oversize_int_liter() {
+    let input = "1000000000000000000";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(result.is_err());
+}
+
+#[test]
+fn can_parse_bool_liter() {
+    let input = "true";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(matches!(result, Ok((expr, _)) if expr == Expr::BoolLiter(true)));
+}
+
+#[test]
+fn can_parse_char_literals() {
+    let input = "\'e\'";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(matches!(result, Ok((expr, _)) if expr == Expr::CharLiter('e')));
+}
+
+#[test]
+fn can_parse_escape_chars() {
+    let input = "\'\\n\'";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(matches!(result, Ok((expr, _)) if expr == Expr::CharLiter('\n')));
+}
+
+#[test]
+fn cannot_parse_invalid() {
+    let input = "\'£\'";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(result.is_err());
+    // assert!(matches!(result, Ok((expr, _)) if expr == Expr::CharLiter('\n')));
+}
+
+#[test]
+fn can_parse_string_literals() {
+    let input = "\"hello\"";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(matches!(result, Ok((expr, _)) if expr == Expr::StrLiter("hello".parse().unwrap())));
+}
+
+#[test]
+fn can_parse_string_literals_with_escape() {
+    let input = "\"a\\0b\"";
+    let result = expr_atom_literal().parse(input).into_result();
+    assert!(matches!(result, Ok((expr, _)) if expr == Expr::StrLiter("a\0b".parse().unwrap())));
 }
--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,179 +1,212 @@
-use chumsky::error::Rich;
-use chumsky::prelude::{
-    any, choice, custom, end, just, none_of, one_of, skip_then_retry_until, SimpleSpan,
-};
-use chumsky::IterParser;
-use chumsky::{extra, text, Parser};
-
-pub type Span = SimpleSpan<usize>;
-
-pub type Spanned<T> = (T, Span);
-
-fn is_wacc_keyword(ident: &str) -> bool {
-    matches!(
-        ident,
-        "true"
-            | "false"
-            | "null"
-            | "len"
-            | "ord"
-            | "chr"
-            | "int"
-            | "bool"
-            | "char"
-            | "string"
-            | "pair"
-            | "begin"
-            | "end"
-            | "is"
-            | "skip"
-            | "read"
-            | "free"
-            | "return"
-            | "exit"
-            | "print"
-            | "println"
-            | "if"
-            | "then"
-            | "else"
-            | "fi"
-            | "while"
-            | "do"
-            | "done"
-            | "newpair"
-            | "call"
-            | "fst"
-            | "snd"
-    )
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Token<'src> {
-    IntToken(i32),
-    StrToken(&'src str),
-    CharToken(char),
-    Ident(&'src str),
-    Bool(bool),
-    Op(&'src str),
-    Keyword(&'src str),
-}
-
-impl<'src> std::fmt::Display for Token<'src> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            Token::IntToken(n) => write!(f, "Int {}", n),
-            Token::StrToken(s) => write!(f, "{}", s),
-            Token::CharToken(c) => write!(f, "{}", c),
-            Token::Ident(id) => write!(f, "{}", id),
-            Token::Bool(b) => write!(f, "{}", b),
-            Token::Op(op) => write!(f, "{}", op),
-            Token::Keyword(kw) => write!(f, "{}", kw),
-        }
-    }
-}
-
-// fn tag(input: &str) -> impl Parser<&str, &str, extra::Err<Rich<char>>> {
-//     just(input).padded_by(comments().repeated()).padded()
+// use chumsky::error::Rich;
+// use chumsky::prelude::{
+//     any, choice, custom, end, just, none_of, one_of, skip_then_retry_until, SimpleSpan,
+// };
+// use chumsky::IterParser;
+// use chumsky::{extra, text, Parser};
+//
+// pub type Span = SimpleSpan<usize>;
+//
+// pub type Spanned<T> = (T, Span);
+//
+// fn is_wacc_keyword(ident: &str) -> bool {
+//     matches!(
+//         ident,
+//         "true"
+//             | "false"
+//             | "null"
+//             | "len"
+//             | "ord"
+//             | "chr"
+//             | "int"
+//             | "bool"
+//             | "char"
+//             | "string"
+//             | "pair"
+//             | "begin"
+//             | "end"
+//             | "is"
+//             | "skip"
+//             | "read"
+//             | "free"
+//             | "return"
+//             | "exit"
+//             | "print"
+//             | "println"
+//             | "if"
+//             | "then"
+//             | "else"
+//             | "fi"
+//             | "while"
+//             | "do"
+//             | "done"
+//             | "newpair"
+//             | "call"
+//             | "fst"
+//             | "snd"
+//     )
 // }
-
-fn lexer<'src>(
-) -> impl Parser<'src, &'src str, Vec<(Token<'src>, Span)>, extra::Err<Rich<'src, char, Span>>> {
-    let num_token = one_of("+-")
-        .or_not()
-        .then(text::int(10))
-        .to_slice()
-        .from_str()
-        .unwrapped()
-        .map(Token::IntToken);
-
-    // let escaped_char = just('\\')
-    //     .then(choice((
-    //         just('\\').to('\\'),
-    //         just('"').to('\"'),
-    //         just('b').to('\x08'),
-    //         just('f').to('\x0C'),
-    //         just('n').to('\n'),
-    //         just('r').to('\r'),
-    //         just('t').to('\t'),
-    //     )))
-    //     .ignored()
-    //     .boxed();
-
-    // let char_set = none_of("\"\'\\").ignored().or(escaped_char);
-
-    // let str_token = char_set
-    //     .repeated()
-    //     .delimited_by(just('\"'), just('\"'))
-    //     .to_slice()
-    //     .map(Token::StrToken);
-
-    // A parser for operators
-    let op = one_of("+-!*%/>=<&|")
-        .repeated()
-        .at_least(1)
-        .to_slice()
-        .map(Token::Op);
-
-    let comments = just("#")
-        .then(any().and_is(just('\n').not()).repeated())
-        .padded();
-
-    let token = num_token.or(op);
-
-    token
-        .map_with(|tok, e| (tok, e.span()))
-        .padded_by(comments.repeated())
-        .padded()
-        .repeated()
-        .collect::<Vec<_>>()
-}
-
-pub fn work(s: &str) -> Vec<Token> {
-    let result = lexer()
-        .parse(s)
-        .into_result()
-        .unwrap()
-        .iter()
-        .map(|(x, _)| x.clone())
-        .collect::<Vec<_>>();
-    println!("{:?}", result);
-    result
-}
-
-#[test]
-fn can_lex_single_digit() {
-    let input = "0";
-    assert_eq!(work(input), vec![Token::IntToken(0)]);
-}
-
-#[test]
-fn can_lex_multiple_digit_number() {
-    let input = "245";
-    assert_eq!(work(input), vec![Token::IntToken(245)]);
-}
-
-#[test]
-fn can_lex_multiple_numbers_with_comments() {
-    let input = "-123   +234 442 881";
-    assert_eq!(
-        work(input),
-        vec![
-            Token::IntToken(-123),
-            Token::IntToken(234),
-            Token::IntToken(442),
-            Token::IntToken(881)
-        ]
-    )
-}
-
-#[test]
-fn can_lex_string_literals() {
-    let input = r#""hello""#;
-    assert_eq!(work(input), vec![Token::StrToken("hello")]);
-}
-
-#[test]
-fn can_lex_escape_characters() {
-    let input = "\"hel\\\'lo\"";
-    assert_eq!(work(input), vec![Token::StrToken("hel\'lo")]);
-}
+//
+// #[derive(Debug, Clone, PartialEq)]
+// pub enum Token<'src> {
+//     IntToken(i32),
+//     StrToken(&'src str),
+//     CharToken(char),
+//     Ident(&'src str),
+//     Bool(bool),
+//     Op(&'src str),
+//     Keyword(&'src str),
+// }
+//
+// impl<'src> std::fmt::Display for Token<'src> {
+//     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+//         match self {
+//             Token::IntToken(n) => write!(f, "Int {}", n),
+//             Token::StrToken(s) => write!(f, "{}", s),
+//             Token::CharToken(c) => write!(f, "{}", c),
+//             Token::Ident(id) => write!(f, "{}", id),
+//             Token::Bool(b) => write!(f, "{}", b),
+//             Token::Op(op) => write!(f, "{}", op),
+//             Token::Keyword(kw) => write!(f, "{}", kw),
+//         }
+//     }
+// }
+//
+// // fn tag(input: &str) -> impl Parser<&str, &str, extra::Err<Rich<char>>> {
+// //     just(input).padded_by(comments().repeated()).padded()
+// // }
+//
+// fn lexer<'src>(
+// ) -> impl Parser<'src, &'src str, Vec<(Token<'src>, Span)>, extra::Err<Rich<'src, char, Span>>> {
+//     let num_token = one_of("+-")
+//         .or_not()
+//         .then(text::int(10))
+//         .to_slice()
+//         .from_str()
+//         .unwrapped()
+//         .map(Token::IntToken);
+//
+//     let preceding_escape = just('\\').ignored();
+//
+//     let escaped_char = choice((
+//         just('\\').just('0'),
+//         just('\\').just('\\'),
+//         just('\\').just('\t'),
+//     ));
+//
+//     // let to_escape = |c: Option<char>| match c {
+//     //     Some('\\') => Some('\\'),
+//     //     Some('\"') => Some('\"'),
+//     //     Some('\'') => Some('\''),
+//     //     Some('b') => Some('\x08'),
+//     //     Some('f') => Some('\x0C'),
+//     //     Some('n') => Some('\n'),
+//     //     Some('r') => Some('\r'),
+//     //     Some('t') => Some('\t'),
+//     //     Some('0') => Some('\0'),
+//     //     _ => None,
+//     // };
+//
+//     let graphic_ascii = none_of("\\\'\"")
+//         .filter(|c: &char| c.is_ascii() && c >= &' ')
+//         .to_slice();
+//
+//     let legal_char = graphic_ascii.or(escaped_char);
+//
+//     let char_token = just('\'')
+//         .ignore_then(legal_char)
+//         .then_ignore(just('\''))
+//         .to_slice()
+//         .map(|c: &str| Token::CharToken(c.chars().nth(1).unwrap()));
+//
+//     // let char_token = char_str.map(Token::CharToken);
+//
+//     // let str_token = just('\"')
+//     //     .ignore_then(char_str.repeated())
+//     //     .then_ignore(just('\"'))
+//     //     .to_slice()
+//     //     .map(Token::StrToken);
+//
+//     // let char_set = none_of("\"\'\\").ignored().or(escaped_char);
+//
+//     // let str_token = char_set
+//     //     .repeated()
+//     //     .delimited_by(just('\"'), just('\"'))
+//     //     .to_slice()
+//     //     .map(Token::StrToken);
+//
+//     // A parser for operators
+//     let op = one_of("+-!*%/>=<&|")
+//         .repeated()
+//         .at_least(1)
+//         .to_slice()
+//         .map(Token::Op);
+//
+//     let comments = just("#")
+//         .then(any().and_is(just('\n').not()).repeated())
+//         .padded();
+//
+//     let token = num_token.or(char_token).or(op);
+//
+//     token
+//         .map_with(|tok, e| (tok, e.span()))
+//         .padded_by(comments.repeated())
+//         .padded()
+//         .repeated()
+//         .collect::<Vec<_>>()
+// }
+//
+// pub fn work(s: &str) -> Vec<Token> {
+//     let result = lexer()
+//         .parse(s)
+//         .into_result()
+//         .unwrap()
+//         .iter()
+//         .map(|(x, _)| x.clone())
+//         .collect::<Vec<_>>();
+//     println!("{:?}", result);
+//     result
+// }
+//
+// #[test]
+// fn can_lex_single_digit() {
+//     let input = "0";
+//     assert_eq!(work(input), vec![Token::IntToken(0)]);
+// }
+//
+// #[test]
+// fn can_lex_multiple_digit_number() {
+//     let input = "245";
+//     assert_eq!(work(input), vec![Token::IntToken(245)]);
+// }
+//
+// #[test]
+// fn can_lex_multiple_numbers_with_comments() {
+//     let input = "-123   +234 442 881";
+//     assert_eq!(
+//         work(input),
+//         vec![
+//             Token::IntToken(-123),
+//             Token::IntToken(234),
+//             Token::IntToken(442),
+//             Token::IntToken(881)
+//         ]
+//     )
+// }
+// #[test]
+// fn can_lex_char() {
+//     let input = "\'e\'";
+//     assert_eq!(work(input), vec![Token::CharToken('e')]);
+// }
+//
+// #[test]
+// fn can_lex_escape() {
+//     let input = "\'\\0\'";
+//     assert_eq!(work(input), vec![Token::CharToken('\0')]);
+// }
+//
+// #[test]
+// fn can_lex_a_string() {
+//     let input = "\"hello\"";
+//     assert_eq!(work(input), vec![Token::StrToken("hello")]);
+// }
