836b473d066ed49e593ad6c4d30276679a7b13ad
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-02-02 18:42:08 +0000

    feat: added atomic expr parsing.

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,5 @@
+use crate::Spanned;
+
 #[derive(PartialEq, Clone, Debug)]
 pub enum Expr {
     // Atomic Literals
@@ -5,4 +7,35 @@ pub enum Expr {
     BoolLiter(bool),
     CharLiter(char),
     StrLiter(String),
+    PairLiter,
+    Ident(String),
+    ArrayElem(String, Vec<Spanned<Expr>>),
+    UnaryApp(UnaryOperator, Box<Spanned<Expr>>),
+    BinaryApp(Box<Spanned<Expr>>, BinaryOperator, Box<Spanned<Expr>>),
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum UnaryOperator {
+    Bang,
+    Negative,
+    Len,
+    Ord,
+    Chr,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum BinaryOperator {
+    Mul,
+    Div,
+    Modulo,
+    Add,
+    Sub,
+    Gt,
+    Gte,
+    Lt,
+    Lte,
+    Eq,
+    Neq,
+    And,
+    Or,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use chumsky::prelude::SimpleSpan;
+
 mod ast;
 mod parser;
+
+pub type Span = SimpleSpan<usize>;
+
+pub type Spanned<T> = (T, Span);
 
 fn main() {
     println!("hello world");
