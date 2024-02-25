8b4e8ac7bb20dad8711804c8146da1d0d423e783
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-02-25 13:28:59 +0000

    chore: abstracted stmt generation

--- src/code_generator/stmt.rs
+++ src/code_generator/stmt.rs
@@ -1,4 +1,4 @@
-use crate::ast::{Expr, Lvalue, Rvalue, ScopedStmt, Stmt, Type};
+use crate::ast::{Expr, Ident, Lvalue, Rvalue, ScopedStmt, Stmt, Type};
 use crate::code_generator::asm::AsmLine::{Directive, Instruction};
 use crate::code_generator::asm::InstrOperand::Reg;
 use crate::code_generator::asm::MemoryReferenceImmediate::OffsetImm;
@@ -10,8 +10,8 @@ use crate::code_generator::asm::{
 use crate::code_generator::clib_functions::{PRINT_LABEL_FOR_STRING, SYS_EXIT_LABEL};
 use crate::code_generator::def_libary::Directives;
 use crate::code_generator::x86_generate::Generator;
-use crate::new_spanned;
 use crate::symbol_table::{Offset, ScopeTranslator};
+use crate::{new_spanned, Spanned};
 use std::process::exit;
 
 impl Generator for ScopedStmt {
@@ -112,85 +112,20 @@ impl Generator for Stmt {
             Stmt::Print(print_type, (exp, _)) => {
                 Self::generate_stmt_print(scope, code, regs, aux, print_type, exp)
             }
-            Stmt::Exit((exit_val, _)) => generate_stat_exit(scope, code, regs, exit_val),
+            Stmt::Exit((exit_val, _)) => {
+                Self::generate_stmt_exit(scope, code, regs, exit_val);
+            }
             Stmt::Serial(statement1, statement2) => {
-                statement1.0.generate(scope, code, regs, aux);
-                statement2.0.generate(scope, code, regs, aux);
+                Self::generate_stmt_serial(scope, code, regs, aux, statement1, statement2);
             }
             Stmt::Declare((type_, _), (lvalue_, _), rvalue) => {
-                // a declare statement is equivalent to an assign statement with Lvalue type LIdent
-                // its stack space must have already been allocated.
-                Stmt::Assign(
-                    type_.clone(),
-                    new_spanned(Lvalue::LIdent(new_spanned(lvalue_.clone()))),
-                    rvalue.clone(),
-                )
-                .generate(scope, code, regs, aux);
+                Self::generate_stmt_declare(scope, code, regs, aux, type_, lvalue_, rvalue);
             }
             Stmt::Assign(type_, (lvalue, _), (rvalue, _)) => {
-                // regs[0] = rvalue
-                rvalue.generate(scope, code, regs, aux);
-
-                // store value in regs[0] to that of lvalue
-                let (target_reg, offset, scale) =
-                    lvalue.generate(scope, code, &regs[1..], type_.clone());
-
-                code.codes.push(Instruction(Instr::BinaryInstr(
-                    BinaryInstruction::new_single_scale(
-                        InstrType::Mov,
-                        scale,
-                        Reg(regs[0]),
-                        InstrOperand::Reference(MemoryReference {
-                            imm: Some(OffsetImm(offset)),
-                            base_reg: Some(target_reg),
-                            shift_unit_reg: None,
-                            shift_cnt: None,
-                        }),
-                    ),
-                )));
+                Self::generate_stmt_assign(scope, code, &regs, aux, type_, lvalue, rvalue);
             }
             Stmt::If((cond, _), st1, st2) => {
-                let true_label = code.get_control_label();
-                let exit_if_label = code.get_control_label();
-
-                // r[0] = evaluate if-condition
-                cond.generate(scope, code, regs, aux);
-
-                // cmpq $1, r[0]
-                code.codes.push(Instruction(Instr::BinaryInstr(
-                    BinaryInstruction::new_single_scale(
-                        InstrType::Cmp,
-                        Scale::default(),
-                        InstrOperand::Imm(1),
-                        InstrOperand::Reg(regs[0]),
-                    ),
-                )));
-
-                // je true_label
-                code.codes
-                    .push(Instruction(Instr::UnaryControl(UnaryNotScaled::new(
-                        InstrType::Jump(Some(ConditionCode::EQ)),
-                        InstrOperand::LabelRef(true_label.clone()),
-                    ))));
-
-                // st2.body
-                st2.generate(scope, code, regs, aux);
-
-                // jmp exit_if_label
-                code.codes
-                    .push(Instruction(Instr::UnaryControl(UnaryNotScaled::new(
-                        InstrType::Jump(None),
-                        InstrOperand::LabelRef(exit_if_label.clone()),
-                    ))));
-
-                // true_label:
-                code.codes.push(Directive(Directives::Label(true_label)));
-
-                // st1.body
-                st1.generate(scope, code, regs, aux);
-
-                // exit_label:
-                code.codes.push(Directive(Directives::Label(exit_if_label)))
+                Self::generate_stmt_if(scope, code, regs, aux, cond, st1, st2);
             }
             _ => todo!(),
         }
@@ -237,45 +172,158 @@ impl Stmt {
                 InstrOperand::LabelRef(String::from(print_label)),
             ))));
     }
-}
 
-fn generate_stat_exit(
-    scope: &ScopeTranslator,
-    code: &mut GeneratedCode,
-    regs: &[Register],
-    exp: &Expr,
-) {
-    // reg[0] = exit_value
-    exp.generate(scope, code, regs, ());
+    fn generate_stmt_if(
+        scope: &ScopeTranslator,
+        code: &mut GeneratedCode,
+        regs: &[Register],
+        aux: (),
+        cond: &Expr,
+        st1: &ScopedStmt,
+        st2: &ScopedStmt,
+    ) {
+        let true_label = code.get_control_label();
+        let exit_if_label = code.get_control_label();
 
-    // move result into the rax register
-    code.codes.push(AsmLine::Instruction(Instr::BinaryInstr(
-        BinaryInstruction::new_single_scale(
-            InstrType::Mov,
-            Scale::default(),
-            Reg(regs[0]),
-            Reg(RESULT_REG),
-        ),
-    )));
+        // r[0] = evaluate if-condition
+        cond.generate(scope, code, regs, aux);
 
-    // move result stored in rax into the rdi register
-    code.codes.push(AsmLine::Instruction(Instr::BinaryInstr(
-        BinaryInstruction::new_single_scale(
-            InstrType::Mov,
-            Scale::default(),
-            Reg(RESULT_REG),
-            Reg(Rdi),
-        ),
-    )));
+        // cmpq $1, r[0]
+        code.codes.push(Instruction(Instr::BinaryInstr(
+            BinaryInstruction::new_single_scale(
+                InstrType::Cmp,
+                Scale::default(),
+                InstrOperand::Imm(1),
+                InstrOperand::Reg(regs[0]),
+            ),
+        )));
 
-    // call predefined exit
-    code.required_clib.insert(CLibFunctions::SystemExit);
+        // je true_label
+        code.codes
+            .push(Instruction(Instr::UnaryControl(UnaryNotScaled::new(
+                InstrType::Jump(Some(ConditionCode::EQ)),
+                InstrOperand::LabelRef(true_label.clone()),
+            ))));
 
-    // add instruction dependency: system exit
-    code.codes
-        .push(Instruction(Instr::UnaryInstr(UnaryInstruction::new_unary(
-            InstrType::Call,
-            Scale::default(),
-            InstrOperand::LabelRef(String::from(SYS_EXIT_LABEL)),
-        ))));
+        // st2.body
+        st2.generate(scope, code, regs, aux);
+
+        // jmp exit_if_label
+        code.codes
+            .push(Instruction(Instr::UnaryControl(UnaryNotScaled::new(
+                InstrType::Jump(None),
+                InstrOperand::LabelRef(exit_if_label.clone()),
+            ))));
+
+        // true_label:
+        code.codes.push(Directive(Directives::Label(true_label)));
+
+        // st1.body
+        st1.generate(scope, code, regs, aux);
+
+        // exit_label:
+        code.codes.push(Directive(Directives::Label(exit_if_label)))
+    }
+
+    fn generate_stmt_assign(
+        scope: &ScopeTranslator,
+        code: &mut GeneratedCode,
+        regs: &&[Register],
+        aux: (),
+        type_: &Type,
+        lvalue: &Lvalue,
+        rvalue: &Rvalue,
+    ) {
+        // regs[0] = rvalue
+        rvalue.generate(scope, code, regs, aux);
+
+        // store value in regs[0] to that of lvalue
+        let (target_reg, offset, scale) = lvalue.generate(scope, code, &regs[1..], type_.clone());
+
+        code.codes.push(Instruction(Instr::BinaryInstr(
+            BinaryInstruction::new_single_scale(
+                InstrType::Mov,
+                scale,
+                Reg(regs[0]),
+                InstrOperand::Reference(MemoryReference {
+                    imm: Some(OffsetImm(offset)),
+                    base_reg: Some(target_reg),
+                    shift_unit_reg: None,
+                    shift_cnt: None,
+                }),
+            ),
+        )));
+    }
+
+    fn generate_stmt_declare(
+        scope: &ScopeTranslator,
+        code: &mut GeneratedCode,
+        regs: &[Register],
+        aux: (),
+        type_: &Type,
+        lvalue_: &Ident,
+        rvalue: &Spanned<Rvalue>,
+    ) {
+        // a declare statement is equivalent to an assign statement with Lvalue type LIdent
+        // its stack space must have already been allocated.
+        Stmt::Assign(
+            type_.clone(),
+            new_spanned(Lvalue::LIdent(new_spanned(lvalue_.clone()))),
+            rvalue.clone(),
+        )
+        .generate(scope, code, regs, aux);
+    }
+
+    fn generate_stmt_serial(
+        scope: &ScopeTranslator,
+        code: &mut GeneratedCode,
+        regs: &[Register],
+        aux: (),
+        statement1: &Box<Spanned<Stmt>>,
+        statement2: &Box<Spanned<Stmt>>,
+    ) {
+        statement1.0.generate(scope, code, regs, aux);
+        statement2.0.generate(scope, code, regs, aux);
+    }
+
+    fn generate_stmt_exit(
+        scope: &ScopeTranslator,
+        code: &mut GeneratedCode,
+        regs: &[Register],
+        exit_val: &Expr,
+    ) {
+        // reg[0] = exit_value
+        exit_val.generate(scope, code, regs, ());
+
+        // move result into the rax register
+        code.codes.push(AsmLine::Instruction(Instr::BinaryInstr(
+            BinaryInstruction::new_single_scale(
+                InstrType::Mov,
+                Scale::default(),
+                Reg(regs[0]),
+                Reg(RESULT_REG),
+            ),
+        )));
+
+        // move result stored in rax into the rdi register
+        code.codes.push(AsmLine::Instruction(Instr::BinaryInstr(
+            BinaryInstruction::new_single_scale(
+                InstrType::Mov,
+                Scale::default(),
+                Reg(RESULT_REG),
+                Reg(Rdi),
+            ),
+        )));
+
+        // call predefined exit
+        code.required_clib.insert(CLibFunctions::SystemExit);
+
+        // add instruction dependency: system exit
+        code.codes
+            .push(Instruction(Instr::UnaryInstr(UnaryInstruction::new_unary(
+                InstrType::Call,
+                Scale::default(),
+                InstrOperand::LabelRef(String::from(SYS_EXIT_LABEL)),
+            ))));
+    }
 }
